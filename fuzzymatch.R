a197b7314e7197258f4e95e07124b88d5641f9f4
matthew p robertson <35159349+mpr1255@users.noreply.github.com>
2023-02-04 15:45:21 +1100

    add the loops, functions, benchmarks

--- fuzzymatch.R
+++ fuzzymatch.R
@@ -8,6 +8,10 @@ library(microbenchmark)
 here <- here()
 
 plan(multisession, workers = 8)
+plan(multisession, workers = 1, gc = TRUE)
+plan()
+furrr::workers()
+furrr::furrr_options()
 
 
 # Load a list of files
@@ -24,21 +28,74 @@ xml_files[1] %>%
     as.data.frame() %>%
     head()
 
+getFullText <- function(file) {
+    file %>%
+        xml2::read_xml() %>%
+        xml2::xml_text()
+}
+# What happens if we try one string on one file?
+stringdist::afind(target_strings[1], getFullText(xml_files[1]), window = nchar(target_strings[1]), method = "running_cosine")
 
-# Try the string
+# What happens if we try one string on all files? First, try a for loop
+forLoop <- function(f_length) {
+    for (i in 1:f_length) {
+        file_text <- getFullText(xml_files[i])
+        res <- stringdist::afind(file_text, target_strings[1], window = nchar(target_strings[1]), method = "running_cosine")
+        location <- res$location
+        distance <- res$distance
+        match <- res$match
+        context <- substr(file_text, as.integer(location) - 70, as.integer(location) + 70)
+        res2 <- as.data.table(cbind(target_strings[1], location, distance, match, context))
+        res2 <- cbind(xml_files[i], res2)
+        names(res2) <- c("file", "target_string", "string_location", "string_distance", "matching_string", "context")
+        if (i == 1) {
+            all_res <- res2
+        } else {
+            all_res <- rbind(all_res, res2)
+        }
+    }
+}
+
+# But we need to run it on all strings, so let's try a nested for loop
+nestedForLoop <- function(f_length) {
+    for (i in 1:f_length) {
+        file_text <- getFullText(xml_files[i])
+        for (j in 1:length(target_strings)) {
+            res <- stringdist::afind(file_text, target_strings[j], window = nchar(target_strings[j]), method = "running_cosine")
+            location <- res$location
+            distance <- res$distance
+            match <- res$match
+            context <- substr(file_text, as.integer(location) - 70, as.integer(location) + 70)
+            res2 <- as.data.table(cbind(target_strings[j], location, distance, match, context))
+            res2 <- cbind(xml_files[i], res2)
+            names(res2) <- c("file", "target_string", "string_location", "string_distance", "matching_string", "context")
+            if (i == 1 & j == 1) {
+                all_res <- res2
+            } else {
+                all_res <- rbind(all_res, res2)
+            }
+        }
+    }
+    return(all_res)
+}
+
+# Do the benchmarks
+microbenchmark(
+    forLoop(1),
+    nestedForLoop(1),
+    forLoop(10),
+    nfl_res <- nestedForLoop(10),
+    forLoop(length(xml_files)),
+    nfl_res <- nestedForLoop(length(xml_files)),
+    times = 1
+)
 
 
-
-
-
-
-
-
+# Now let's try vectorise it.
 
 # Set up main functions
 getStringMatches <- function(file_text, target_string) {
     # target_string <- target_strings[1]
-
     res <- stringdist::afind(file_text, target_string, window = nchar(target_string), method = "running_cosine")
     location <- res$location
     distance <- res$distance
@@ -51,8 +108,7 @@ getStringMatches <- function(file_text, target_string) {
 getFullMatch <- function(file, target_strings) {
     # file <- xml_files[1]
     # file_text <- fread(file, sep = NULL, header = FALSE)
-    file_text <- xml2::read_xml(file) %>%
-        xml2::xml_text()
+    file_text <- getFullText(file)
 
     res <- rbindlist(future_map(target_strings, ~ getStringMatches(file_text, .x)))
     res <- cbind(file, res)
@@ -61,10 +117,63 @@ getFullMatch <- function(file, target_strings) {
 }
 
 possibly_getFullMatch <- possibly(getFullMatch, otherwise = NA)
-options(datatable.prettyprint.char = 20000L)
+# options(datatable.prettyprint.char = 20000L)
+
+# Now do the same benchmarks as the for loops
+microbenchmark(
+    null <- map_df(xml_files[1], ~ possibly_getFullMatch(.x, target_strings)),
+    null <- future_map_dfr(xml_files[1], ~ possibly_getFullMatch(.x, target_strings)),
+    null <- map_df(xml_files[1:10], ~ possibly_getFullMatch(.x, target_strings)),
+    null <- future_map_dfr(xml_files[1:10], ~ possibly_getFullMatch(.x, target_strings)),
+    null <- map_df(xml_files, ~ possibly_getFullMatch(.x, target_strings)),
+    null <- future_map_dfr(xml_files, ~ possibly_getFullMatch(.x, target_strings)),
+    times = 1
+)
 
 microbenchmark(
-    all_res <- future_map_dfr(xml_files[1:100], ~ possibly_getFullMatch(.x, target_strings)),
-    all_res <- map_df(xml_files[1:100], ~ possibly_getFullMatch(.x, target_strings)),
+    null <- map_dfr(xml_files, ~ possibly_getFullMatch(.x, target_strings)),
+    nfl_res <- nestedForLoop(length(xml_files)),
+    times = 1
+)
+
+
+target_strings2 <- rep(target_strings, 5)
+xml_files2 <- rep(xml_files, 5)
+
+cross2(target_strings, xml_files) %>%
+    rbindlist() %>%
+    nrow()
+cross2(target_strings2, xml_files2) %>%
+    rbindlist() %>%
+    nrow()
+
+
+nestedForLoop2 <- function(f_length) {
+    # f_length <- length(xml_files2)
+    for (i in 1:f_length) {
+        file_text <- getFullText(xml_files2[i])
+        for (j in 1:length(target_strings2)) {
+            res <- stringdist::afind(file_text, target_strings2[j], window = nchar(target_strings2[j]), method = "running_cosine")
+            location <- res$location
+            distance <- res$distance
+            match <- res$match
+            context <- substr(file_text, as.integer(location) - 70, as.integer(location) + 70)
+            res2 <- as.data.table(cbind(target_strings2[j], location, distance, match, context))
+            res2 <- cbind(xml_files2[i], res2)
+            names(res2) <- c("file", "target_string", "string_location", "string_distance", "matching_string", "context")
+            if (i == 1 & j == 1) {
+                all_res <- res2
+            } else {
+                all_res <- rbind(all_res, res2)
+            }
+        }
+    }
+    return(all_res)
+}
+
+
+microbenchmark(
+    null <- future_map_dfr(xml_files2, ~ possibly_getFullMatch(.x, target_strings2)),
+    nfl_res <- nestedForLoop2(length(xml_files2)),
     times = 1
 )
